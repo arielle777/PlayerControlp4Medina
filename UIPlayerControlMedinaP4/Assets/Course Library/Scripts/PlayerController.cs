f941bb9d263a411f3a9786809562e2c9464f600e
arielle777 <723653@student.dusd.net>
2025-03-06 11:43:01 -0800

    ended off on tint
    I ended finisahing the tint

--- UIPlayerControlMedinaP4/Assets/Course Library/Scripts/PlayerController.cs
+++ UIPlayerControlMedinaP4/Assets/Course Library/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // We'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
 
     }
