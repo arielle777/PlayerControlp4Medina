4f3c0ff61824df094bb7388f11b82c193fcc68fd
arielle777 <723653@student.dusd.net>
2025-03-18 20:14:43 -0700

    FINISHED PLAYER CONTROL CHALLENGE

--- UIPlayerControlMedinaP4/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ UIPlayerControlMedinaP4/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
-    public float rotationSpeed;
-    public float verticalInput;
-   public float horizontalInput;
-    public float forwardInput;
+    public float speed = 25.0f;
+    public float rotationSpeed = 25.0f;
+    public float verticalInput = 25.0f;
+ 
+   
 
     // Start is called before the first frame update
     void Start()
@@ -21,12 +21,11 @@ public class PlayerControllerX : MonoBehaviour
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
-        
+
         // move the plane forward at a constant rate
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
-        transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime * verticalInput);
     }
 }
